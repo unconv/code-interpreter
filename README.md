d0b7f8a5fe2d30ae68dcc7a18951e231e0ba395c
unconv <unconventionalcoding@gmail.com>
2023-10-15 11:49:56 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ChatGPT CodeInterpreter in PHP
+
+This is ChatGPT CodeInterpreter for the terminal made in PHP. You can ask it to create charts, analyze CSV files or anything, really.
+
+## Quick Start
+
+```shell
+$ php interpreter.php
+```
+
+Requires PHP and Python
---
+++ interpreter.php
@@ -0,0 +1,104 @@
+<?php
+if ( PHP_SAPI !== "cli" || isset( $_SERVER['HTTP_USER_AGENT'] ) ) {
+    die( "This program is intended to be used from the terminal.\n" );
+}
+
+require( __DIR__ . "/ChatGPT.php" );
+
+class PythonResult {
+    public function __construct(
+        public string $output,
+        public int $result_code,
+    ) {}
+}
+
+function run_python_code( string $code ): PythonResult {
+    $temp_file = "/tmp/code.py";
+
+    if( ! file_put_contents( $temp_file, $code ) ) {
+        throw new \Exception( "Unable to write code file" );
+    }
+
+    $output = [];
+    $result_code = NULL;
+    exec( "python3 ".escapeshellarg( $temp_file )." 2>&1", $output, $result_code );
+
+    if( file_exists( $temp_file ) ) unlink( $temp_file );
+
+    return new PythonResult(
+        output: implode( "\n", $output ),
+        result_code: $result_code,
+    );
+}
+
+function input( string $message = "" ): string {
+    echo $message;
+    $stdin = fopen( "php://stdin", "r" );
+    $line = fgets( $stdin );
+    fclose( $stdin );
+    return trim( $line );
+}
+
+/**
+ * Run python code
+ *
+ * @param string $code The code to run. Code must have a print statement in the end that prints out the relevant return value
+ */
+function python( string $code ): string {
+    $styled_code = "";
+    $code_rows = explode( "\n", $code );
+
+    foreach( $code_rows as $row ) {
+        $styled_code .= "# " . str_pad( $row, 45 ) . " #\n";
+    }
+
+    echo "\n";
+    echo "#################################################\n";
+    echo "# I WANT TO RUN THIS PYTHON CODE:               #\n";
+    echo $styled_code;
+    echo "#################################################\n";
+
+    do {
+        $answer = input( "\nDo you want to run this code? (yes/no)" );
+    } while ( ! in_array( $answer, ["yes", "no"] ) );
+
+    if( $answer != "yes" ) {
+        echo "\nSKIPPED RUNNING CODE\n";
+        return "User rejected code. Please ask for changes or what to do next.";
+    }
+
+    $result = run_python_code( $code );
+
+    return json_encode( [
+        "output" => $result->output,
+        "result_code" => $result->result_code,
+    ] );
+}
+
+if( ! file_exists( __DIR__ . "/data/" ) ) {
+    mkdir( __DIR__ . "/data/" );
+}
+
+$chatgpt = new ChatGPT( getenv( "OPENAI_API_KEY" ) );
+$chatgpt->smessage( "You are an AI assistant that can run Python code in order to answer the user's question. You can access a folder called 'data/' from the Python code to read or write files. Write visualizations and charts into a file if possible. When creating links to files in the data directory in your response, use the format [link text](data/filename)" );
+$chatgpt->add_function( "python" );
+
+echo "################################################\n";
+echo "# PHP CodeInterpreter by Unconventional Coding #\n";
+echo "################################################\n\n";
+
+echo "GPT: Hello! I am the PHP CodeInterpreter! What would you like to do?
+You: ";
+
+while( true ) {
+    $message = input();
+
+    if( in_array( $message, ["exit", "quit", "stop"] ) ) {
+        echo "\nGPT: Thanks!\n";
+        exit;
+    }
+
+    $chatgpt->umessage( $message );
+
+    echo "\n\nGPT: " . $chatgpt->response()->content . "\nYou: ";
+}
