584d95f5091fb3445df210a9ab34af50bb4e2d12
Rubens Peculis <rubens@otivo.com>
2023-10-17 19:12:56 +1100

    feat: clean up and added flag to set model

--- interpreter.php
+++ interpreter.php
@@ -1,42 +1,50 @@
 <?php
-if ( PHP_SAPI !== "cli" || isset( $_SERVER['HTTP_USER_AGENT'] ) ) {
-    die( "This program is intended to be used from the terminal.\n" );
+if (PHP_SAPI !== "cli" || isset($_SERVER['HTTP_USER_AGENT'])) {
+    die("This program is intended to be used from the terminal.\n");
 }
 
-require( __DIR__ . "/ChatGPT.php" );
+require(__DIR__ . "/ChatGPT.php");
 
-class PythonResult {
+class PythonResult
+{
     public function __construct(
         public string $output,
-        public int $result_code,
-    ) {}
+        public int    $result_code,
+    )
+    {
+    }
 }
 
-function run_python_code( string $code ): PythonResult {
+/**
+ * @throws Exception
+ */
+function run_python_code(string $code): PythonResult
+{
     $temp_file = "/tmp/code.py";
-
-    if( ! file_put_contents( $temp_file, $code ) ) {
-        throw new \Exception( "Unable to write code file" );
+    $pythonCommand = getenv("PYTHON_COMMAND") ?? "python3";
+    if (!file_put_contents($temp_file, $code)) {
+        throw new Exception("Unable to write code file");
     }
 
     $output = [];
     $result_code = NULL;
-    exec( "python3 ".escapeshellarg( $temp_file )." 2>&1", $output, $result_code );
+    exec($pythonCommand . ' ' . escapeshellarg($temp_file) . ' 2>&1', $output, $result_code);
 
-    if( file_exists( $temp_file ) ) unlink( $temp_file );
+    if (file_exists($temp_file)) unlink($temp_file);
 
     return new PythonResult(
-        output: implode( "\n", $output ),
-        result_code: $result_code,
+        output: implode("\n", $output),
+        result_code: (int)$result_code,
     );
 }
 
-function input( string $message = "" ): string {
+function input(string $message = ""): string
+{
     echo $message;
-    $stdin = fopen( "php://stdin", "r" );
-    $line = fgets( $stdin );
-    fclose( $stdin );
-    return trim( $line );
+    $stdin = fopen("php://stdin", "r");
+    $line = fgets($stdin);
+    fclose($stdin);
+    return trim($line);
 }
 
 /**
@@ -44,12 +52,13 @@ function input( string $message = "" ): string {
  *
  * @param string $code The code to run. Code must have a print statement in the end that prints out the relevant return value
  */
-function python( string $code ): string {
+function python(string $code): string
+{
     $styled_code = "";
-    $code_rows = explode( "\n", $code );
+    $code_rows = explode("\n", $code);
 
-    foreach( $code_rows as $row ) {
-        $styled_code .= "# " . str_pad( $row, 45 ) . " #\n";
+    foreach ($code_rows as $row) {
+        $styled_code .= "# " . str_pad($row, 45) . " #\n";
     }
 
     echo "\n";
@@ -59,29 +68,44 @@ function python( string $code ): string {
     echo "#################################################\n";
 
     do {
-        $answer = input( "\nDo you want to run this code? (yes/no)" );
-    } while ( ! in_array( $answer, ["yes", "no"] ) );
+        $answer = input("\nDo you want to run this code? (yes/no)");
+    } while (!in_array($answer, ["yes", "no"]));
 
-    if( $answer != "yes" ) {
+    if ($answer != "yes") {
         echo "\nSKIPPED RUNNING CODE\n";
         return "User rejected code. Please ask for changes or what to do next.";
     }
 
-    $result = run_python_code( $code );
+    try {
+        $result = run_python_code($code);
+    } catch (Exception $e) {
+        return json_encode([
+            "output" => $e->getMessage(),
+            "result_code" => $e->getCode() ?? -1,
+        ]);
+    }
 
-    return json_encode( [
+    return json_encode([
         "output" => $result->output,
         "result_code" => $result->result_code,
-    ] );
+    ]);
 }
 
-if( ! file_exists( __DIR__ . "/data/" ) ) {
-    mkdir( __DIR__ . "/data/" );
+if (!file_exists(__DIR__ . "/data/")) {
+    mkdir(__DIR__ . "/data/");
 }
 
-$chatgpt = new ChatGPT( getenv( "OPENAI_API_KEY" ) );
-$chatgpt->smessage( "You are an AI assistant that can run Python code in order to answer the user's question. You can access a folder called 'data/' from the Python code to read or write files. Write visualizations and charts into a file if possible. When creating links to files in the data directory in your response, use the format [link text](data/filename)" );
-$chatgpt->add_function( "python" );
+$chatGPT = new ChatGPT(getenv("OPENAI_API_KEY"));
+if (getenv("MODEL")) {
+    $chatGPT->set_model(getenv("MODEL"));
+}
+$chatGPT->smessage("You are an AI assistant that can run Python code in order to answer the user's question. You can access a folder called 'data/' from the Python code to read or write files. Write visualizations and charts into a file if possible. When creating links to files in the data directory in your response, use the format [link text](data/filename)");
+try {
+    $chatGPT->add_function("python");
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage() . "\n";
+    exit;
+}
 
 echo "################################################\n";
 echo "# PHP CodeInterpreter by Unconventional Coding #\n";
@@ -90,15 +114,20 @@ echo "################################################\n\n";
 echo "GPT: Hello! I am the PHP CodeInterpreter! What would you like to do?
 You: ";
 
-while( true ) {
+while (true) {
     $message = input();
 
-    if( in_array( $message, ["exit", "quit", "stop"] ) ) {
+    if (in_array($message, ["exit", "quit", "stop"])) {
         echo "\nGPT: Thanks!\n";
         exit;
     }
 
-    $chatgpt->umessage( $message );
+    $chatGPT->umessage($message);
 
-    echo "\n\nGPT: " . $chatgpt->response()->content . "\nYou: ";
+    try {
+        echo "\n\nGPT: " . $chatGPT->response()->content . "\nYou: ";
+    } catch (Exception $e) {
+        echo "\n\nGPT: Something when wrong " . $e->getMessage() . "\n";
+        exit;
+    }
 }
